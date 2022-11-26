947020ed4d5bf84030456d545f806764e008b0b8
Jaison2003 <jaisonsamjiju93@gmail.com>
2022-11-26 12:08:57 +0530

    success

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,25 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { AddcourseComponent } from './addcourse/addcourse.component';
+import { ViewAllCourseComponent } from './view-all-course/view-all-course.component';
+import { NavbarComponent } from './navbar/navbar.component';
+import { Router, RouterModule } from '@angular/router';
+
+const myroutes=[{path:"",component: AddcourseComponent},
+{path:"view-all-course",component:ViewAllCourseComponent }]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AddcourseComponent,
+    ViewAllCourseComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(myroutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
